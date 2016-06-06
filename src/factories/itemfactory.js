a62dca1cbb581aea730db7fa41f561f4094f6f83
mraatika <raatikaine@gmail.com>
2016-06-06 17:23:37 +0300

    Fixed item creation (this.game.game =>  this.game)

--- src/factories/itemfactory.js
+++ src/factories/itemfactory.js
@@ -1,16 +1,30 @@
 import Item from 'sprites/item';
 import gameConfig from 'json!assets/config/gameconfig.json';
 
+/**
+ * @class ItemFactory
+ * @description A factory class for creating items
+ */
 export default class ItemFactory {
 
+    /**
+     * @constructor
+     * @param       {Phaser.Game} game
+     * @return      {ItemFactory}
+     */
     constructor(game) {
         this.game = game;
     }
 
+    /**
+     * Create an item
+     * @param  {string} objId An id for locating item properties from config file
+     * @return {Phaser.Sprite}
+     */
     create(objId) {
         const centerPoint = gameConfig.map.tileSize / 2;
         const itemProps = gameConfig.items[objId];
-        const itemSprite = new Item(this.game.game, centerPoint, centerPoint, itemProps);
+        const itemSprite = new Item(this.game, centerPoint, centerPoint, itemProps);
 
         itemSprite.center();
 
