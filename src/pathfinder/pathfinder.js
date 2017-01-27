ccda7f5529c43f4914d79183ab9c7b55444b350a
Miitri Raatikainen <raatikaine@gmail.com>
2017-01-27 15:40:03 +0200

    Store commit for pathfinder fix (enemies move through other enemies)

--- src/pathfinder/pathfinder.js
+++ src/pathfinder/pathfinder.js
@@ -1,5 +1,7 @@
+import {Point} from 'phaser';
+import {each, map} from 'lodash';
 import EasyStar from 'easystarjs';
-import {each} from 'lodash';
+import MapUtils from 'common/maputils';
 
 /**
  * @class PathFinder
@@ -16,6 +18,7 @@ export default class PathFinder {
     constructor(game, props = {}) {
         this.game = game;
         this._easyStar = new EasyStar.js();
+        this._grid = null;
         each(props, this._setProp);
     }
 
@@ -30,10 +33,27 @@ export default class PathFinder {
         const layerIndex = map.getLayerIndex(layerName);
         const layer = map.layers[layerIndex];
         const tiles = layer.data;
+        const grid = this._formGrid(tiles);
 
-        this._grid = this._formGrid(tiles);
-        this._easyStar.setGrid(this._grid);
+        this._tiles = tiles;
+        this._setGrid(grid);
+
         this._easyStar.setAcceptableTiles(acceptableTiles);
+    }
+
+    /**
+     * Update obstacle positions of obstacles not derived from map data (actors etc.)
+     * @param  {Phaser.Point[]} obstacles
+     */
+    updateObstaclePositions(obstacles) {
+        const positions = map(obstacles, obstacle => {
+            const tilePosition = MapUtils.getTilePositionByCoordinates(new Point(obstacle.x, obstacle.y));
+            return [tilePosition.x, tilePosition.y];
+        });
+
+        const grid = this._formGrid(this._tiles, positions);
+
+        this._setGrid(grid);
     }
 
     /**
@@ -62,20 +82,34 @@ export default class PathFinder {
      * Form two dimensional array from given array
      * @private
      * @param   {Array} tiles
+     * @param   {Array} [obstacles = []]
      * @return  {Array[]}
      */
-    _formGrid(tiles) {
+    _formGrid(tiles, obstacles = []) {
         const grid = [];
 
         for (let r = 0; r < tiles.length; r++) {
             grid[r] = [];
 
             for (let c = 0; c < tiles[r].length; c++) {
-                grid[r][c] = tiles[r][c].index;
+                grid[r][c] = this._isInObstacles(obstacles, r, c) ? 99 : tiles[r][c].index;
             }
         }
 
         return grid;
+    }
+
+    _setGrid(grid) {
+        this._grid = grid;
+        this._easyStar.setGrid(grid);
+    }
+
+    _isInObstacles(obstacles, row, col) {
+        for (let i in obstacles) {
+            if (obstacles[i][0] == row && obstacles[i][1] == col) return true;
+        }
+
+        return false;
     }
 
     /**
