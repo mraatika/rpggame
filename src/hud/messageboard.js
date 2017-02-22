98a4a77b9e320f712d1794dfb3bf25295fbdfeed
Miitri Raatikainen <raatikaine@gmail.com>
2017-02-22 09:56:55 +0200

    Messageboard w/ styling as DOM overlay

--- src/hud/messageboard.js
+++ src/hud/messageboard.js
@@ -2,61 +2,95 @@ import { BoundedStack } from 'datastructures';
 import SpriteBase from '../sprites/spritebase';
 import EventTypes from '../constants/eventtypes';
 
+const BOARD_SIDE_MARGIN = 10;
+const BOARD_TOP_MARGIN = 5;
+
+/**
+ * Create user readable message from an event object
+ * @param {GameEvent} event
+ * @returns {string}
+ */
 function createMessageFromEvent(event) {
+    const { actor = {} } = event;
+    const className = actor.isPlayerControlled ? 'actor' : 'enemy';
+
     switch (event.type) {
     case EventTypes.LOG_EVENT:
         return event.text;
     case EventTypes.ATTACK_EVENT:
-        return `${event.actor.name} is attacking ${event.target.name} with ${event.attack}`;
+        return `<span class="${className}">${event.actor.name}</span> is attacking ${event.target.name} with ${event.attack}`;
     case EventTypes.LOOT_EVENT:
         {
             const lootedItems = event.loot.items.map(i => i.name);
-            return `${event.actor.name} looted ${event.loot.gold} gold and ${lootedItems.length} items ${lootedItems.length ? `(${lootedItems.join(', ')})` : ''}`;
+            return `<span class="${className}">${event.actor.name}</span> looted ${event.loot.gold} gold and ${lootedItems.length} items ${lootedItems.length ? `(${lootedItems.join(', ')})` : ''}`;
         }
     case EventTypes.DEFEND_EVENT:
-        return `${event.actor.name} defends with ${event.defence}`;
+        return `<span class="${className}">${event.actor.name}</span> defends with ${event.defence}`;
     case EventTypes.DAMAGE_EVENT:
-        return `${event.actor.name} took ${event.damage} damage`;
+        return `<span class="${className}">${event.actor.name}</span> took ${event.damage} damage`;
     case EventTypes.ACTOR_KILLED_EVENT:
-        return `${event.actor.name} was killed`;
+        return `<span class="${className}">${event.actor.name}</span> was killed`;
     case EventTypes.END_TURN_EVENT:
-        return event.actor.isPlayerControlled ? `${event.actor.name} ended their turn` : null;
+        return event.actor.isPlayerControlled ? `<span class="${className}">${event.actor.name}</span> ended their turn` : null;
     default:
         return '';
     }
 }
 
+/**
+ * Create a div DOM element positioned on top of this sprite to host
+ * the message texts
+ * @private
+ * @memberOf MessageBoard
+ */
+function createMessageBoardDOMElement() {
+    const div = global.document.createElement('div');
+
+    div.id = 'messageboard';
+
+    div.style.font = '12px Arial, sans-serif';
+    div.style.color = '#ffffff';
+    div.style.position = 'absolute';
+    div.style.left = `${this.x + 8}px`;
+    div.style.top = `${this.y + 8}px`;
+    div.style.width = `${this.width - (2 * BOARD_SIDE_MARGIN)}px`;
+    div.style.height = `${this.height - (2 * BOARD_TOP_MARGIN)}px`;
+    div.style.padding = `${BOARD_TOP_MARGIN}px ${BOARD_SIDE_MARGIN}px`;
+    div.style.pointerEvents = 'none';
+
+    global.document.getElementById('phaser-game').appendChild(div);
+}
+
+/**
+ * Render all messages
+ * @private
+ * @memberOf MessageBoard
+ */
 function renderMessages() {
-    const message = this.messages.container.slice(0).reverse().join('\n');
-    const style = {
-        font: '12px Arial, sans-serif',
-        fill: '#ffffff',
-        boundsAlignH: 'left',
-        boundsAlignV: 'top',
-        wordWrap: true,
-        wordWrapWidth: this.width - 10,
-    };
+    if (!global.document.getElementById('messageboard')) {
+        createMessageBoardDOMElement.call(this);
+    }
 
-    if (this.messageText) this.messageText.destroy();
+    const message = this.messages.container.slice(0).reverse().join('<br>');
+    const div = global.document.getElementById('messageboard');
 
-    this.messageText = this.game.add.text(0, 0, '', style);
-    this.messageText.setTextBounds(this.x + 5, this.y + 5, this.width - 5, this.height - 5);
-    this.messageText.setText(message);
+    div.innerHTML = message;
 }
 
 
 /**
+ * @exports
  * @class MessageBoard
- * @description
+ * @description A gui component for displaying action log to the player
  * @extends {SpriteBase}
  */
 export default class MessageBoard extends SpriteBase {
     /**
-     * @constructor
-     * @param       {Game} game
-     * @param       {number} x
-     * @param       {number} y
-     * @return      {MessageBoard}
+     * Creates an instance of MessageBoard.
+     * @param {Game} game
+     * @param {number} x
+     * @param {number} y
+     * @memberOf MessageBoard
      */
     constructor(game, x, y) {
         super(game, x, y, 'overlay');
@@ -68,6 +102,11 @@ export default class MessageBoard extends SpriteBase {
         renderMessages.call(this);
     }
 
+    /**
+     * Form message from event object and add it to the action log
+     * @param {GameEvent} event
+     * @memberOf MessageBoard
+     */
     logEvent(event) {
         const message = createMessageFromEvent(event);
         if (message) {
@@ -75,6 +114,11 @@ export default class MessageBoard extends SpriteBase {
         }
     }
 
+    /**
+     * Add message to the action log
+     * @param {string} event
+     * @memberOf MessageBoard
+     */
     addMessage(message) {
         this.messages.add(message);
         renderMessages.call(this);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-// eslint-disable-next-line
 import 'pixi';
-// eslint-disable-next-line
 import 'phaser';
 import Game from './game/game';
 import gameConfig from './config/gameconfig.json';
+import '../public/styles.css';
 
 const game = new Game(gameConfig.screen.width, gameConfig.screen.height);
 
