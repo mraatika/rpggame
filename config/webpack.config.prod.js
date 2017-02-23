35f3496ebb2d73c16ccafebf4a904d0b4061ad3d
Miitri Raatikainen <raatikaine@gmail.com>
2017-02-23 14:53:11 +0200

    Added html loader to prod config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -167,6 +167,13 @@ module.exports = {
       {
         test: /phaser-arcade-physics\.js$/,
         loader: 'expose-loader?Phaser'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {
+            minimize: true
+        }
       }
     ]
   },
