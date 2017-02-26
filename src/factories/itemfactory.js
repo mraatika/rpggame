285b780fdd35b236ebcf38d963e60ad9a8515408
Miitri Raatikainen <raatikaine@gmail.com>
2017-02-26 17:09:13 +0200

    Item is no longer a sprite. Added frame name to items to be used as css sprite class name.

--- src/factories/itemfactory.js
+++ src/factories/itemfactory.js
@@ -2,32 +2,18 @@ import Item from '../sprites/item';
 import gameConfig from '../config/gameconfig.json';
 
 /**
+ * @exports
  * @class ItemFactory
  * @description A factory class for creating items
  */
 export default class ItemFactory {
-
-    /**
-     * @constructor
-     * @param       {Phaser.Game} game
-     * @return      {ItemFactory}
-     */
-    constructor(game) {
-        this.game = game;
-    }
-
     /**
      * Create an item
      * @param  {string} objId An id for locating item properties from config file
-     * @return {Phaser.Sprite}
+     * @returns {Item}
      */
     create(objId) {
-        const centerPoint = gameConfig.map.tileSize / 2;
-        const itemProps = gameConfig.items[objId];
-        const itemSprite = new Item(this.game, centerPoint, centerPoint, itemProps);
-
-        itemSprite.center();
-
-        return itemSprite;
+        const itemProps = Object.assign({}, gameConfig.items[objId], { id: objId });
+        return new Item(itemProps);
     }
 }
--- src/sprites/item.js
+++ src/sprites/item.js
@@ -1,42 +1,54 @@
-import SpriteBase from './spritebase';
+import Events from '../events/events';
 
 /**
+ * @exports
  * @class Item
  * @description
- * @extends {SpriteBase}
  */
-export default class Item extends SpriteBase {
+export default class Item {
     /**
-     * @constructor
-     * @param       {Game} game
-     * @param       {number} x
-     * @param       {number} y
-     * @return      {Item}
+     * Creates an instance of Item.
+     * @param {Object} [props={}]
+     * @memberOf Item
      */
-    constructor(game, x, y, props = {}) {
-        super(game, x, y, 'items', props.frame);
+    constructor(props = {}) {
+        this.id = props.id;
+        this.name = props.name;
 
         this.attackModifier = props.attackModifier || 0;
         this.defenceModifier = props.defenceModifier || 0;
         this.movementModifier = props.movementModifier || 0;
-        this.name = props.name;
 
-        this.isEquippable = props.isEquippable || false;
+        this.frame = props.frame;
+        this.itemGroup = props.itemGroup;
+
+        this.isEquippable = props.isEquippable || true;
         this.isEquipped = false;
     }
 
     /**
      * Equip this item if it's equippable
+     * @param {boolean} [condition=true] If false will unequip
+     * @fires Events#ItemEquippedEvent
+     * @returns {undefined}
+     * @memberOf Item
      */
     equip() {
-        if (this.isEquippable) return;
-        this.isEquipped = true;
+        if (this.isEquippable) {
+            this.isEquipped = true;
+            new Events.ItemEquippedEvent(this, true).dispatch();
+        }
     }
 
     /**
-     * Unequip this item
+     * Unequip this item if equipped
+     * @fires Events#ItemEquippedEvent
+     * @memberOf Item
      */
     unequip() {
-        this.isEquipped = false;
+        if (this.isEquipped) {
+            this.isEquipped = false;
+            new Events.ItemEquippedEvent(this, false).dispatch();
+        }
     }
 }
