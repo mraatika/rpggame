61ab6b3f8f1ceac4377ab9ed38c926c67290ba28
Miitri Raatikainen <raatikaine@gmail.com>
2017-02-28 20:21:00 +0200

    Initiate root vue instance on first mount call so it won't attempt it when running tests.

--- src/vue/vuerenderer.js
+++ src/vue/vuerenderer.js
@@ -1,4 +1,6 @@
-import VueApp from './vueapp';
+import Vue from 'vue';
+
+let vueApp;
 
 /**
  * Render Vue component to vue-domain div
@@ -8,10 +10,12 @@ import VueApp from './vueapp';
  * @returns {Vue.Component} Created component instance
  */
 export default function mount(VueComponent, props = {}) {
+    // initiate vue app when first component mounts
+    if (!vueApp) { vueApp = new Vue({ el: '#vue-domain' }); }
     // create instance pass props as propsData. All received props should be
     // defined in the component itself since it will not register undefined props
     // even if present in propsData
-    const instance = new VueComponent({ parent: VueApp, propsData: props });
+    const instance = new VueComponent({ parent: vueApp, propsData: props });
     // mount after props are added
     instance.$mount();
     global.document.getElementById('vue-domain').appendChild(instance.$el);
