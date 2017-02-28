abd7026aeb50d6a7a4a82152f5b18f6d4c7ee052
Miitri Raatikainen <raatikaine@gmail.com>
2017-02-28 20:22:06 +0200

    Mock console.info to hide annoying vue "tips".

--- config/jest/setup.js
+++ config/jest/setup.js
@@ -1,4 +1,6 @@
 (function(global) {
     var PIXI = require('phaser-ce/build/custom/pixi.js');
     window.PIXI = PIXI;
+    // because of annoying vue "tips" >x(
+    console.info = () => {};
 }(this));
