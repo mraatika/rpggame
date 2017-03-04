e18d28c4c1e658b54fe2026a372e42ded1cc378e
Miitri Raatikainen <raatikaine@gmail.com>
2017-03-04 13:36:53 +0200

    Refactored for better readability.

--- src/factories/item.js
+++ src/factories/item.js
@@ -4,65 +4,66 @@ import gameConfig from '../config/gameconfig.json';
 const config = gameConfig.items;
 
 /**
- * Default item props
- * @type {Object}
- */
-const itemDefaults = {
-    id: '',
-    name: '',
-    chance: 1,
-    attackModifier: 0,
-    defenceModifier: 0,
-    movementModifier: 0,
-    itemGroup: null,
-    description: '',
-    frame: '',
-    isEquippable: true,
-    isEquipped: false,
-};
-
-/**
  * @exports
  * @name Item
  * A factory function for creating item objects
  * @returns {Item}
  */
 export default function createItem(props = {}) {
-    return Object.assign(
-        {},
-        // default props
-        itemDefaults,
+    /**
+     * Public properties
+     */
+    const publicProperties = Object.assign(
+        {
+            id: '',
+            name: '',
+            chance: 1,
+            attackModifier: 0,
+            defenceModifier: 0,
+            movementModifier: 0,
+            itemGroup: null,
+            description: '',
+            frame: '',
+            isEquippable: true,
+            isEquipped: false,
+        },
         // props from game config
         config[props.id],
-        // item properties
+        // props given as argument
         props,
-        // item methods
-        {
-            /**
-             * Equip this item if it's equippable
-             * @param {boolean} [condition=true] If false will unequip
-             * @fires Events#ItemEquippedEvent
-             * @returns {undefined}
-             * @memberOf Item
-             */
-            equip() {
-                if (this.isEquippable) {
-                    this.isEquipped = true;
-                    new Events.ItemEquippedEvent(this, true).dispatch();
-                }
-            },
+    );
 
-            /**
-             * Unequip this item if equipped
-             * @fires Events#ItemEquippedEvent
-             * @memberOf Item
-             */
-            unequip() {
-                if (this.isEquipped) {
-                    this.isEquipped = false;
-                    new Events.ItemEquippedEvent(this, false).dispatch();
-                }
-            },
+    const methods = {
+        /**
+         * Equip this item if it's equippable
+         * @param {boolean} [condition=true] If false will unequip
+         * @fires Events#ItemEquippedEvent
+         * @returns {undefined}
+         * @memberOf Item
+         */
+        equip() {
+            if (this.isEquippable) {
+                this.isEquipped = true;
+                new Events.ItemEquippedEvent(this, true).dispatch();
+            }
         },
+
+        /**
+         * Unequip this item if equipped
+         * @fires Events#ItemEquippedEvent
+         * @memberOf Item
+         */
+        unequip() {
+            if (this.isEquipped) {
+                this.isEquipped = false;
+                new Events.ItemEquippedEvent(this, false).dispatch();
+            }
+        },
+    };
+
+    return Object.assign(
+        {},
+        publicProperties,
+        methods,
     );
 }
